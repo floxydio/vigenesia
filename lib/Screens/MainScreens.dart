901c14a1a3de4509cd282c5b32c06a4a1ff9fb0f
floxydio <diooktar@gmail.com>
2021-07-07 13:11:26 +0700

    3

--- lib/Screens/MainScreens.dart
+++ lib/Screens/MainScreens.dart
@@ -1,8 +1,10 @@
+import 'package:acidahv2/Models/Motivasi_Model.dart';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'Login.dart';
 import 'package:acidahv2/Constant/const.dart';
+import 'dart:convert';
 
 class MainScreens extends StatefulWidget {
   final String nama;
@@ -14,8 +16,56 @@ class MainScreens extends StatefulWidget {
 
 class _MainScreensState extends State<MainScreens> {
   String baseurl = url;
-
+  var dio = Dio();
+  List<MotivasiModel> ass = [];
   TextEditingController titleController = TextEditingController();
+
+  Future<dynamic> sendMotivasi(String isi) async {
+    Map<String, dynamic> body = {
+      "isi_motivasi": isi,
+    };
+
+    try {
+      Response response =
+          await dio.post("$baseurl/aci/api/dev/POSTmotivasi/", data: body);
+
+      print("Respon -> ${response.data} + ${response.statusCode}");
+
+      return response;
+    } catch (e) {
+      print("Error di -> $e");
+    }
+  }
+
+  List<MotivasiModel> listproduk = [];
+
+  Future<List<MotivasiModel>> getData() async {
+    var response = await dio.get('$baseurl/aci/api/Get_motivasi');
+
+    print(" ${response.data}");
+    if (response.statusCode == 200) {
+      var getUsersData = response.data as List;
+      var listUsers =
+          getUsersData.map((i) => MotivasiModel.fromJson(i)).toList();
+      return listUsers;
+    } else {
+      throw Exception('Failed to load');
+    }
+  }
+
+  Future<Null> _refresh() {
+    return getData().then((_listproduk) {
+      setState(() => listproduk = _listproduk);
+    });
+  }
+
+  TextEditingController isiController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,41 +78,85 @@ class _MainScreensState extends State<MainScreens> {
             child: Padding(
               padding: const EdgeInsets.only(left: 10.0, right: 10.0),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment
-                    .center, // <-- Berfungsi untuk  atur nilai X jadi tengah
-                children: [
-                  SizedBox(
-                    height: 40,
-                  ),
+                  mainAxisAlignment: MainAxisAlignment
+                      .center, // <-- Berfungsi untuk  atur nilai X jadi tengah
+                  children: [
+                    SizedBox(
+                      height: 40,
+                    ),
 
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Hallo  ${widget.nama}",
-                        style: TextStyle(
-                            fontSize: 22, fontWeight: FontWeight.w500),
-                      ),
-                      TextButton(
-                          child: Icon(Icons.logout),
-                          onPressed: () {
-                            Navigator.pop(context);
-                            Navigator.push(
-                                context,
-                                new MaterialPageRoute(
-                                    builder: (BuildContext context) =>
-                                        new Login()));
-                          })
-                    ],
-                  ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Hallo  ${widget.nama}",
+                          style: TextStyle(
+                              fontSize: 22, fontWeight: FontWeight.w500),
+                        ),
+                        TextButton(
+                            child: Icon(Icons.logout),
+                            onPressed: () {
+                              Navigator.pop(context);
+                              Navigator.push(
+                                  context,
+                                  new MaterialPageRoute(
+                                      builder: (BuildContext context) =>
+                                          new Login()));
+                            })
+                      ],
+                    ),
 
-                  SizedBox(height: 50), // <-- Kasih Jarak Tinggi : 50px
+                    SizedBox(height: 20), // <-- Kasih Jarak Tinggi : 50px
+                    FormBuilderTextField(
+                      controller: isiController,
+                      name: "isi_motivasi",
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          contentPadding: EdgeInsets.only(left: 10),
+                          labelText: "Your Post..."),
+                    ),
+                    ElevatedButton(
+                        onPressed: () async {
+                          await sendMotivasi(isiController.text.toString());
+                          print("Sukses");
+                        },
+                        child: Text("Submit")),
 
-                  SizedBox(
-                    height: 40,
-                  ),
-                ],
-              ),
+                    SizedBox(
+                      height: 40,
+                    ),
+
+                    FutureBuilder(
+                        future: getData(),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<List<MotivasiModel>> snapshot) {
+                          if (snapshot.hasData) {
+                            return Column(
+                              children: [
+                                for (var item in snapshot.data)
+                                  RefreshIndicator(
+                                    onRefresh: () {},
+                                    child: Stack(
+                                      children: [
+                                        ListView(
+                                          physics:
+                                              AlwaysScrollableScrollPhysics(),
+                                          shrinkWrap: true,
+                                          children: [Text(item.isiMotivasi)],
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                              ],
+                            );
+                          } else if (snapshot.hasData &&
+                              snapshot.data.isEmpty) {
+                            return Text("No Data");
+                          } else {
+                            return null;
+                          }
+                        })
+                  ]),
             ),
           ),
         ),
